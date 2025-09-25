9c1541bc75daa6f777bf0c8f50231f077ebffcca
sravanthpasupuleti <sravanthpasupuleti.gmail.com>
2025-09-25 17:25:49 +0530

    Only jdbc

---
+++ Demo0/src/main/java/com/scaleupindia/repository/OwnerRepository.java
@@ -0,0 +1,18 @@
+package com.scaleupindia.repository;
+
+import java.util.List;
+
+import com.scaleupindia.dto.OwnerDTO;
+
+
+public interface OwnerRepository {
+	void saveOwner(OwnerDTO owner);
+
+	OwnerDTO findOwner(int ownerId);
+
+	void updatePetDetails(int ownerId, String petName);
+
+	void deleteOwner(int ownerId);
+
+	List<OwnerDTO> findAllOwners();
+}
